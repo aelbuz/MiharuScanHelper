9a9b50b9f312e58c018e5beefcf7fc2230effd94
Aydın Elbüz <aelbuz37@gmail.com>
2022-02-12 22:43:15 +0300

    Fixed ActualHeight binding exception caused by ParsedTextBoxHeightConverter.

--- Miharu Scan Helper/FrontEnd/TextEntry/ParsedTextBoxHeightConverter.cs
+++ Miharu Scan Helper/FrontEnd/TextEntry/ParsedTextBoxHeightConverter.cs
@@ -1,17 +1,19 @@
-﻿
-using System.Windows.Data;
+﻿using System.Windows.Data;
 
 namespace Miharu.FrontEnd.TextEntry
 {
-	public class ParsedTextBoxHeightConverter : IValueConverter {
+    public class ParsedTextBoxHeightConverter : IValueConverter
+    {
+        private const int Offset = 6;
 
-		public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-		
-			return System.Convert.ToDouble(value) -6;
-		}
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return System.Convert.ToDouble(value) < Offset ? 0 : System.Convert.ToDouble(value) - 6;
+        }
 
-		public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-			return null;
-		}
-	}
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
 }
